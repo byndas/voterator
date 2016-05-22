997d6c5d499639d79179e78cce55c180dadbface
Dan <danvolosnikov@gmail.com>
2016-05-22 23:15:14 +0000

    form is ready

--- server.js
+++ server.js
@@ -1,33 +1,44 @@
 'use strict';
 
-var express = require('express');
-var routes = require('./app/routes/index.js');
+var express  = require('express');
+var app      = express();
 var mongoose = require('mongoose');
 var passport = require('passport');
-var session = require('express-session');
+var flash    = require('connect-flash');
+var path = require('path');
+var morgan       = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser   = require('body-parser');
+var session      = require('express-session');
 
-var app = express();
-require('dotenv').load();
-require('./app/config/passport')(passport);
+// configuration ===============================================================
+mongoose.connect(process.env.MONGO_URI || "mongodb://" + process.env.IP + ":27017/imgsearch"); // connect to our database
 
-mongoose.connect(process.env.MONGO_URI);
+require('./config/passport')(passport); // pass passport for configuration
 
-app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
-app.use('/public', express.static(process.cwd() + '/public'));
-app.use('/common', express.static(process.cwd() + '/app/common'));
+// set up our express application
+app.use(morgan('dev')); // log every request to the console
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(bodyParser.urlencoded({
+  extended: false
+})); // get information from html forms
 
-app.use(session({
-	secret: 'secretClementine',
-	resave: false,
-	saveUninitialized: true
-}));
+app.set('view engine', 'ejs'); // set up ejs for templating
+app.set('views',path.join(__dirname, 'views'));
 
+// required for passport
+app.use(session({secret: 'myownsecret',
+ 				resave:false,
+    			saveUninitialized:false
+	})); // session secret
 app.use(passport.initialize());
-app.use(passport.session());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
 
-routes(app, passport);
+// routes ======================================================================
+require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
 
-var port = process.env.PORT || 8080;
-app.listen(port,  function () {
-	console.log('Node.js listening on port ' + port + '...');
-});
+// launch ======================================================================
+var port     = process.env.PORT || 8080;
+app.listen(port);
+console.log('The magic happens on port ' + port);
